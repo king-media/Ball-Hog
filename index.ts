66069e05b1a62be6934be4dc03bfc9abdea9df31
InceptionCode <darrellwjr.com@gmail.com>
2022-10-27 15:51:02 -0400

    attempt to fix pathing issue.

--- index.ts
+++ index.ts
@@ -7,14 +7,16 @@ const root = process.env.CWD || process.cwd()
 const app = express()
 
 const resolve = (p: string) => path.resolve(root, p)
-const template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(resolve('/dist')))
 // Add API Routes here.... app.use("/api", apiRoutes)
 app.use('*', async (req, res) => {
   try {
     const url = req.originalUrl
+    const template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
     // @ts-ignore
     const render = (await import('/dist/server/entry-server.js')).render
     const manifest: Record<string, string[]> = (
