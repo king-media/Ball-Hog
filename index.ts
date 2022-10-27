a38e72c697dd04c31913ba7f38f3b365d62e4222
InceptionCode <darrellwjr.com@gmail.com>
2022-10-27 13:06:13 -0400

    fix import issues.

--- index.ts
+++ index.ts
@@ -3,20 +3,10 @@ import path from 'node:path'
 import express from 'express'
 import cors from 'cors'
 
-const isProd = process.env.NODE_ENV === 'production'
 const root = process.env.CWD || process.cwd()
 const app = express()
 
 const resolve = (p: string) => path.resolve(root, p)
-
-const template = isProd
-  ? fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
-  : ''
-
-const manifest: Record<string, string[]> = isProd
-  ? // @ts-ignore
-    (await import('./dist/client/ssr-manifest.json')).default
-  : {}
 
 app.use(cors())
 app.use(express.static(resolve('/dist')))
@@ -24,8 +14,12 @@ app.use(express.static(resolve('/dist')))
 app.use('*', async (req, res) => {
   try {
     const url = req.originalUrl
+    const template = fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
     // @ts-ignore
     const render = (await import('/dist/server/entry-server.js')).render
+    const manifest: Record<string, string[]> = (
+      await import('./dist/client/ssr-manifest.json')
+    ).default
 
     const [appHtml, preloadLinks] = await render(url, manifest)
 
