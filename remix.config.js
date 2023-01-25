5beba10480c18cfcdaba695d516d74bf912499d5
InceptionCode <darrellwjr.com@gmail.com>
2023-01-25 02:12:08 -0500

    why

--- remix.config.js
+++ remix.config.js
@@ -7,6 +7,7 @@ module.exports = {
   server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
 
   appDirectory: 'src',
+  ignoredRouteFiles: ['**/.*'],
   // ignoredRouteFiles: [
   //   '.*',
   //   '**/*.action.*',
