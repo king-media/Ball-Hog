0ef2357e3fbf0b00504c3c3eaa156471a5da923e
InceptionCode <darrellwjr.com@gmail.com>
2023-01-25 02:19:32 -0500

    the 2nd generated link doesn't work. The 1st one does why.

--- remix.config.js
+++ remix.config.js
@@ -3,19 +3,23 @@ const useVercelRuntime = !!VERCEL
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget: useVercelRuntime ? 'vercel' : 'node-cjs',
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: useVercelRuntime ? './server.js' : undefined,
+
   appDirectory: 'src',
+  assetsBuildDirectory: 'public/build',
 
-  serverBuildTarget: 'vercel',
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
-
-  ignoredRouteFiles: ['**/.*'],
-  // ignoredRouteFiles: [
-  //   '.*',
-  //   '**/*.action.*',
-  //   '**/*.loader.*',
-  //   '**/*.meta.*',
-  //   '**/*.story.*',
-  //   '**/*.test.*',
-  //   '**/components/**/*.*',
-  // ],
+  publicPath: '/build/',
+  ignoredRouteFiles: [
+    '.*',
+    '**/*.action.*',
+    '**/*.loader.*',
+    '**/*.meta.*',
+    '**/*.story.*',
+    '**/*.test.*',
+    '**/components/**/*.*',
+  ],
 }
