7a51d38c25eb7eacd801b6f241bb50ce094f0557
InceptionCode <darrellwjr.com@gmail.com>
2023-01-25 01:54:29 -0500

    ignored route files maybe causing error

--- remix.config.js
+++ remix.config.js
@@ -3,25 +3,22 @@ const useVercelRuntime = !!VERCEL
 
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: 'vercel',
-  // serverBuildTarget: useVercelRuntime ? 'vercel' : 'node-cjs',
-  // // When running locally in development mode, we use the built in remix
-  // // server. This does not understand the vercel lambda module format,
-  // // so we default back to the standard build output.
-  // server: useVercelRuntime ? './server.js' : undefined,
+  serverBuildTarget: useVercelRuntime ? 'vercel' : 'node-cjs',
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: useVercelRuntime ? './server.js' : undefined,
 
   appDirectory: 'src',
-  // assetsBuildDirectory: 'public/build',
-  // publicPath: '/build/',
-  ignoredRouteFiles: [
-    '.*',
-    '**/*.action.*',
-    '**/*.loader.*',
-    '**/*.meta.*',
-    '**/*.story.*',
-    '**/*.test.*',
-    '**/components/**/*.*',
-  ],
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
-  ignoredRouteFiles: ['**/.*'],
+  assetsBuildDirectory: 'public/build',
+  publicPath: '/build/',
+  // ignoredRouteFiles: [
+  //   '.*',
+  //   '**/*.action.*',
+  //   '**/*.loader.*',
+  //   '**/*.meta.*',
+  //   '**/*.story.*',
+  //   '**/*.test.*',
+  //   '**/components/**/*.*',
+  // ],
 }
