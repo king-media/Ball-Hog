27f63f423e156281057e625aea655e3b4c10b998
InceptionCode <darrellwjr.com@gmail.com>
2023-02-01 17:09:21 -0500

    added header and navigation

--- src/Layout/header/index.tsx
+++ src/Layout/header/index.tsx
@@ -1,16 +1,39 @@
-import { Box, Container } from '@mui/material'
+import { NavLink, useSearchParams } from '@remix-run/react'
+import { Box, Link as MuiLink } from '@mui/material'
 
-export const Header = (props: any) => (
-  <>
-    <Box className="placeholder-header" padding="4rem">
-      <h1>HEADER</h1>
-    </Box>
-    <Container
-      maxWidth="xl"
-      sx={{ padding: 0 }}
-      className="placeholder-navigation"
-    >
-      <h1>NAVIGATION</h1>
-    </Container>
-  </>
-)
+export const Header = (props: any) => {
+  const [searchParams] = useSearchParams()
+
+  const startDate = searchParams.get('startDate')
+  const endDate = searchParams.get('endDate')
+
+  return (
+    <>
+      <Box className="header" display="flex" justifyContent="center">
+        <img src="/assets/Ball-Hog-Header.png" height="650px" />
+      </Box>
+      <Box
+        display="flex"
+        justifyContent="space-evenly"
+        width="100%"
+        className="navigation"
+      >
+        <MuiLink
+          to={{
+            pathname: '/',
+            search:
+              startDate && endDate
+                ? `?startDate=${startDate}&endDate=${endDate}`
+                : '',
+          }}
+          underline="none"
+          variant="h6"
+          color="lightgray"
+          component={NavLink}
+        >
+          SCORES
+        </MuiLink>
+      </Box>
+    </>
+  )
+}
--- src/routes/game-stats.$gameId.tsx
+++ src/routes/game-stats.$gameId.tsx
@@ -4,5 +4,8 @@ export { loader } from 'src/pages/GameStats/loader'
 import statsStyles from '~/css/stats-styles.css'
 
 export function links() {
-  return [{ rel: 'stylesheet', href: statsStyles }]
+  return [
+    { rel: 'prelaod', href: '/assets/Ball-Hog-Header.png', as: 'image' },
+    { rel: 'stylesheet', href: statsStyles },
+  ]
 }
